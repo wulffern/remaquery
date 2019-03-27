84a4c0adab5d64eb57b4211954b7d2dadb165b5e
Carsten Wulff <carsten@wulff.no>
2019-03-27 19:58:52 +0100

    Added group, listgroup, and category commands

--- rema2query.py
+++ rema2query.py
@@ -30,12 +30,75 @@ import json
 import sys
 import collections
 import datetime
+import click
+from collections import OrderedDict
 
 class rema:
     def __init__(self,file):
         with open(file,"r") as f:
-            jsonobj = json.load(f)        
+            jsonobj = json.load(f)  
+
+        cat_fn = "categories.json"
+        categories = None
+        if(os.path.exists(cat_fn)):
+            with open(cat_fn,"r") as f:
+                categories = json.load(f)
+
+        self.categories = categories
         self.obj = jsonobj
+        self.oformat = "str"
+
+
+    def printGroups(self):
+        groups = dict()
+        transactions = self.obj["TransactionsInfo"]["Transactions"]
+        for t in transactions:
+            for item in t["Receipt"]:
+                groups[item["ProductGroupDescription"]] = " "
+            
+        self.printList(groups)
+
+    def printOrderByCategory(self,maxcount=10):
+        summary = dict()
+
+        if self.categories is None:
+            print("Could not find categories.json. Can't run this command")
+
+        
+        transactions = self.obj["TransactionsInfo"]["Transactions"]
+        for t in transactions:
+            datestr= str(t["PurchaseDate"])
+            d = datetime.datetime.utcfromtimestamp(int(datestr[:-3]))
+            year = d.year
+            month = d.month
+            header_key = str(year) + "-" + str(month)
+            if(header_key not in summary):
+                summary[header_key] = dict()
+            for item in t["Receipt"]:
+                key = item["ProductGroupDescription"]
+                if(key in self.categories):
+                    key = self.categories[key]
+                if(key in summary[header_key]):
+                    summary[header_key][key] += item["Amount"]
+                else:
+                    summary[header_key][key] = item["Amount"]
+        self.printTransactionSummary(summary,maxcount)
+        
+
+    def printTransactionSummary(self,summary,maxcount):
+        data = OrderedDict()
+        for header_key in summary:
+            transactions = summary[header_key]
+            data[header_key] = list()
+            listofTuples = sorted(transactions.items() ,reverse = True,  key=lambda x: x[1])
+            count = 0
+            for s in listofTuples:
+                if(count > maxcount):
+                    continue
+                else:
+                    count += 1
+                data[header_key].append((s[1],s[0]))
+        self.printDictWithTouple(data)
 
     def printOrderByGroup(self,maxcount=10):
         summary = dict()
@@ -46,38 +109,72 @@ class rema:
             year = d.year
             if(d.year not in summary):
                 summary[year] = dict()
-            #print(json.dumps(t["PurchaseDate"],indent=4))
             for item in t["Receipt"]:
-                #print(json.dumps(item,indent=4))
                 key = item["ProductGroupDescription"]
                 if(key in summary[year]):
                     summary[year][key] += item["Amount"]
                 else:
                     summary[year][key] = item["Amount"]
-        for year in summary:
-            transactions = summary[year]
-            listofTuples = sorted(transactions.items() ,reverse = True,  key=lambda x: x[1])
-            count = 0
-            print(str(year) + ":\n")
-            for s in listofTuples:
-                if(count > maxcount):
-                    continue
-                else:
-                    count += 1
-                print("\t%.1f\t%s" %(s[1],s[0]))
-        #return listofTuples
-                #print(json.dumps(item,indent=4))
-                #print("\n\n")
-            
+
+        self.printTransactionSummary(summary,maxcount)
+
+    def printList(self,data):
+        if(self.oformat == "json"):
+            print(json.dumps(data,indent=4))
+        else:
+            for el in data:
+                print(el)
+
+    #- Print function expects the following:
+    # Ordered dictionary headers as keys, where each item in the dict is a touple
+    # The touple needs to be (number, description)
+    def printDictWithTouple(self,data):
+        if(self.oformat == "json"):
+            print(json.dumps(data,indent=4))
+        else:
+            for key in data:
+                print(str(key) + ":")
+                for el in data[key]:
+                    print("\t%.1f\t%s" %(el[0],el[1]))
 
 
-def main(filename):
+
+
+#----------------------------------------------------------
+#- Command line
+#----------------------------------------------------------
+
+@click.group()
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('--oformat',default=str,help="Output format, json or str")
+@click.pass_context
+def cli(ctx,filename,oformat):
+    ctx.ensure_object(dict)
+    #Load the file
     r = rema(filename)
-    summary = r.printOrderByGroup()
-        #print("%.1f\t%s" %(s[1],s[0]))
-    #print(json.dumps(r.obj["TransactionsInfo"]["Transactions"] ,indent=4))
+    r.oformat = oformat
+    ctx.obj['rema'] = r
+    
+    
+@cli.command('group',help="Top items ordered by group")
+@click.pass_context
+@click.option('--maxcount',default=10,help="Number of items to list")
+def group(ctx,maxcount):
+    ctx.obj['rema'].printOrderByGroup(maxcount)
+
+@cli.command('listgroup',help="List all groups")
+@click.pass_context
+def listgroup(ctx):
+    ctx.obj['rema'].printGroups()
 
 
+@cli.command('category',help="Top items ordered by category")
+@click.pass_context
+@click.option('--maxcount',default=10,help="Number of items to list")
+def category(ctx,maxcount):
+    ctx.obj['rema'].printOrderByCategory(maxcount)
+
+    
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    cli(obj = {})
